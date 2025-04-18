66c905a4f164fcba5198cd1f370094b1034181b2
GianES26 <a20224926@pucp.edu.pe>
2025-04-18 12:50:19 -0500

    Primer commit Tarea Clase 4

---
+++ src/main/java/com/example/tarea_clase4_20224926/TareaClase420224926Application.java
@@ -0,0 +1,13 @@
+package com.example.tarea_clase4_20224926;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TareaClase420224926Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TareaClase420224926Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tarea_clase4_20224926/TareaClase420224926ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tarea_clase4_20224926;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaClase420224926ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
